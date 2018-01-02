21a38a4cf61c3727a5487cacaf4f41341cc292b1
swetha <swethagnthn@gmail.com>
2018-01-02 14:23:45 +0530

    scaffold article

---
+++ db/migrate/20180102084012_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.string :name
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20180102084058_remove_article.rb
@@ -0,0 +1,5 @@
+class RemoveArticle < ActiveRecord::Migration[5.1]
+  def change
+  	#drop_table :articles
+  end
+end
